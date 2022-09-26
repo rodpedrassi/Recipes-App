9a9eb644dc42a0e4441cdf6267c349a40614aed9
Luiz Otávio <luizotavio222@gmail.com>
2022-09-26 17:42:16 -0300

    Correção erros Footer

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -6,12 +6,12 @@ import App from '../App';
 describe('Testa a barra de navegação', () => {
   test('testa o botão meal', async () => {
     const { history } = renderWithRouter(<App />, '/foods');
-    userEvent.click(screen.getByTestId('drinks-bottom-btn'))
+    userEvent.click(screen.getByTestId('drinks-bottom-btn'));
     await waitFor(() => {
-      expect(history.location.pathname).toBe('/drinks')
-    }, { timeout: 1000, interval: 100 })
+      expect(history.location.pathname).toBe('/drinks');
+    }, { timeout: 1000, interval: 100 });
 
-    userEvent.click(screen.getByTestId('food-bottom-btn'))
-    expect(history.location.pathname).toBe('/foods')
+    userEvent.click(screen.getByTestId('food-bottom-btn'));
+    expect(history.location.pathname).toBe('/foods');
   });
 });
