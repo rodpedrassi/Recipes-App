b09adc4f39ef1099a2bd32b97ca80cf5ac0a1cb9
Marciel <marcyel.jsclaret17@gmail.com>
2022-09-27 18:44:17 -0300

    provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
+import RecipesProvider from './context/RecipesProvider';
 import Recipes from './pages/Recipes';
 import Drinks from './pages/Drinks';
 import DrinksInProgress from './pages/DrinksInProgress';
@@ -16,21 +17,21 @@ import Favorites from './pages/Favorites';
 function App() {
   return (
     <main className="">
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/meals" component={ Recipes } />
-        <Route exact path="/drinks" component={ Drinks } />
-        <Route exact path="/meals/:id" component={ MealRecipe } />
-        <Route exact path="/drinks/:id" component={ DrinkRecipe } />
-        <Route exact path="/drinks/:id/progress" component={ DrinksInProgress } />
-        <Route exact path="/meals/:id/progress" component={ MealInProgress } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/done-recipes" component={ Done } />
-        <Route exact path="/favorite-recipes" component={ Favorites } />
-      </Switch>
+      <RecipesProvider>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Recipes } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/meals/:id" component={ MealRecipe } />
+          <Route exact path="/drinks/:id" component={ DrinkRecipe } />
+          <Route exact path="/drinks/:id/progress" component={ DrinksInProgress } />
+          <Route exact path="/meals/:id/progress" component={ MealInProgress } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ Done } />
+          <Route exact path="/favorite-recipes" component={ Favorites } />
+        </Switch>
+      </RecipesProvider>
     </main>
-
   );
 }
-
 export default App;
