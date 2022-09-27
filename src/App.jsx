1b9f9dd2ca97ef43c673841321d70dc74bdf4d48
Marciel <marcyel.jsclaret17@gmail.com>
2022-09-27 18:37:50 -0300

    testes header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
 import Recipes from './pages/Recipes';
@@ -16,20 +16,18 @@ import Favorites from './pages/Favorites';
 function App() {
   return (
     <main className="">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/meals" component={ Recipes } />
-          <Route exact path="/drinks" component={ Drinks } />
-          <Route exact path="/meals/:id" component={ MealRecipe } />
-          <Route exact path="/drinks/:id" component={ DrinkRecipe } />
-          <Route exact path="/drinks/:id/progress" component={ DrinksInProgress } />
-          <Route exact path="/meals/:id/progress" component={ MealInProgress } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/done-recipes" component={ Done } />
-          <Route exact path="/favorite-recipes" component={ Favorites } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Recipes } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/meals/:id" component={ MealRecipe } />
+        <Route exact path="/drinks/:id" component={ DrinkRecipe } />
+        <Route exact path="/drinks/:id/progress" component={ DrinksInProgress } />
+        <Route exact path="/meals/:id/progress" component={ MealInProgress } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ Done } />
+        <Route exact path="/favorite-recipes" component={ Favorites } />
+      </Switch>
     </main>
 
   );
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -3,7 +3,7 @@ import Header from '../Components/Header';
 
 function Recipes() {
   return (
-    <div>
+    <div data-testid="section-recipes">
       <Header title="Meals" />
     </div>
   );
